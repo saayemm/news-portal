0a76113eab8d82fa4cb24a418c4fc87c5ef40dc5
Saayemm <sayemdcc@gmail.com>
2025-01-19 18:39:53 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEZDLeQCG1sWdI-uHo67WR2ydSL4IH1FQ",
+  authDomain: "news-portal-1c62b.firebaseapp.com",
+  projectId: "news-portal-1c62b",
+  storageBucket: "news-portal-1c62b.firebasestorage.app",
+  messagingSenderId: "722712132251",
+  appId: "1:722712132251:web:f8729c1f30b5aa7051e43e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
