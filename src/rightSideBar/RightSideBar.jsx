5fc6412786b631fc5cc2409d2c0d020a19318669
Saayemm <sayemdcc@gmail.com>
2025-01-20 10:53:57 +0600

    login

--- src/rightSideBar/RightSideBar.jsx
+++ src/rightSideBar/RightSideBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FaGoogle } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
 
 const RightSideBar = () => {
   return (
     <div>
+      <div className='mb-12'>
       <h2 className='mb-4'>Login With</h2>
       <div className='flex flex-col gap-2'>
             <button 
@@ -19,6 +21,40 @@ const RightSideBar = () => {
                   <span><FaGithub />
                   </span>Login With Github
             </button>
+      </div>
+      </div>
+
+      <div>
+      <div>
+      <h2 className='mb-4'>Find Us On</h2>
+      <div className='flex flex-col'>
+            <a href="">
+            <button 
+                className='flex gap-2 rounded-t-lg items-center 
+                border border-gray-400 px-6 py-2 w-full'>
+                  <span><FaFacebookF />
+                  </span>Facebook
+            </button>
+            </a>
+            <a href="">
+            <button 
+                className='flex gap-2 items-center 
+                border border-gray-400 px-6 py-2 w-full'>
+                  <span><FaFacebookF />
+                  </span>Twitter
+            </button>
+            </a>
+            <a href="">
+            <button 
+                className='flex gap-2 items-center rounded-b-lg
+                border border-gray-400 px-6 py-2 w-full'>
+                  <span><FaFacebookF />
+                  </span>Instagram
+            </button>
+            </a>
+            
+      </div>
+      </div>
       </div>
     </div>
   )
